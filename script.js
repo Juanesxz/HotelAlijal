a81f6037b6d0ee6378aab3986651fd3971c49280
Juanesxz <juaneswilli@hotmail.com>
2021-11-10 21:11:49 -0500

    Inicial

---
+++ script.js
@@ -0,0 +1,6 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js')
+        .then(reg => console.log('Registro de SW exitoso', reg))
+        .catch(err => console.log('Error al registrar el SW', err));
+}
+
